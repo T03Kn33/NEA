369e8d32bd6ca3788365bb2557ab6426a48d4d15
T03Kn33 <toniokedeyi@gmail.com>
2025-03-14 15:51:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -86,6 +86,34 @@ def validate_gap():
  except ValueError:
   return False
 
+def validate_pd():
+ try:
+    pd = pd_entry.get()
+    if pd:
+        if pd.isdigit() and pd > 0:
+            label.config(
+                text=f"Valid input: {input}",
+                foreground="green",
+            )
+            return True
+        else:
+            label.config(
+                text="Input must be a number greater than zero",
+                foreground="red",
+            )
+            return False
+    else:
+        label.config(
+            text="Entry is empty",
+            foreground="red",
+        )
+        return False
+ except TypeError:
+  return False
+
+ except ValueError:
+  return False
+
 class moveCube(tk.Canvas):
  
     def __init__(self, *args, **kwargs):
